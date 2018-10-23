23a41ad32264a02c6a5a021051f0a855bddba074
Tim Broughton <TimBroughton@quickenloans.com>
2018-10-23 12:34:13 -0400

    copied class to seperate file

---
+++ GrandCircusLabOOPCircle/Circle.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrandCircusLabOOPCircle
+{
+    class Circle
+    {
+
+        //private field the Circle class needs to work
+        private double Radius;
+
+
+        //constructor method takes one variable at instantiation
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        //does the math
+        public double CalculateCircumference()
+        {
+            return System.Math.Round(  ((2 * System.Math.PI) * this.Radius), 2);
+        }
+
+        //returns the formatted number
+        public string CalculateFormattedCircumference()
+        {
+            return FormatNumber(CalculateCircumference());
+        }
+
+        //does the math
+        public double CalculateArea()
+        {
+            return System.Math.Round((System.Math.PI * (this.Radius * this.Radius)), 2);
+        }
+
+        //returns the formatted number
+        public string CalculateFormattedArea()
+        {
+            return FormatNumber(CalculateArea());
+        }
+
+
+        //sets number to a string, private because it means nothing to end user - only needed locally
+        private string FormatNumber(double x)
+        {
+            string numberText = "";
+
+            numberText += x;
+
+            return numberText;
+        }
+
+
+    }
+}
--- GrandCircusLabOOPCircle/Program.cs
+++ GrandCircusLabOOPCircle/Program.cs
@@ -14,6 +14,7 @@ namespace GrandCircusLabOOPCircle
 
             Console.WriteLine("Please enter the radius of the circle: ");
 
+            //verify input
             while (true)
             {
                 try
@@ -29,70 +30,22 @@ namespace GrandCircusLabOOPCircle
                 break;
             }
 
+            //instantiate a circle with the radius the user wants
             Circle newCircle = new Circle(radius);
 
+            //use public methods to grab the string answer to circumference
             Console.WriteLine("\nThe circumference is: " + newCircle.CalculateFormattedCircumference());
 
+            //use public methods to grab the string answer to area
             Console.WriteLine("\nThe area is: " + newCircle.CalculateFormattedArea());
 
 
-
+            //keeps console open
             Console.ReadKey();
 
 
         }
     }
-
-
-    //our class Circle------------------------------------------
-    class Circle
-    {
-        private double Radius;
-
-        public Circle(double radius)
-        {
-            Radius = radius;
-        }
-
-        //does the math
-        public double CalculateCircumference()
-        {
-            return System.Math.Round( ((2 * System.Math.PI) * this.Radius), 2 );
-        }
-
-        //returns the formatted number
-        public string CalculateFormattedCircumference()
-        {
-            return FormatNumber(CalculateCircumference());
-        }
-
-        //does the math
-        public double CalculateArea()
-        {
-            return System.Math.Round( (System.Math.PI * (this.Radius * this.Radius)), 2 );
-        }
-
-        //returns the formatted number
-        public string CalculateFormattedArea()
-        {
-            return FormatNumber(CalculateArea());
-        }
-
-
-        //sets number to a string
-        private string FormatNumber(double x)
-        {
-            string numberText = "";
-
-            numberText += x;
-
-            return numberText;
-        }
-
-
-    }
-
-
 
 
 }
